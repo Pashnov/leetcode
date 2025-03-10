c53950d530d35b17da8fc2112ce2d103488012f7
ANDRII PASHNOV <andrii_pashnov@homedepot.com>
2025-03-10 11:38:44 +0100

    3. Longest Substring Without Repeating Characters
    solve 1 more time + Max's solution

--- src/org/axp/medium/_3_LongestSubstringWithoutRepeatingCharacters.java
+++ src/org/axp/medium/_3_LongestSubstringWithoutRepeatingCharacters.java
@@ -14,6 +14,49 @@ public class _3_LongestSubstringWithoutRepeatingCharacters {
     }
 
     public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        if (s.length() == 1) {
+            return 1;
+        }
+        var longestSubstringSize = 1;
+        var startI = 0;
+        main: for (int i = 1; i < s.length(); i++) {
+            for (int j = startI; j < i; j++) {
+                if (s.charAt(j) == s.charAt(i)) {
+                    startI = j + 1;
+                    continue main;
+                }
+            }
+            if (i - startI >= longestSubstringSize) {
+                longestSubstringSize++;
+            }
+        }
+
+        return longestSubstringSize;
+    }
+
+    // Max's
+    public int lengthOfLongestSubstring_(String s) {
+        char[] chars = s.toCharArray();
+        int currentLenght = 0;
+        int maxLength = 0;
+        int l = 0, r = 0;
+        for (int i = 1; i <= chars.length; i++) {
+            r = i;
+            String substring = s.substring(l, r - 1);
+            if (substring.contains(String.valueOf(chars[i]))) {
+                currentLenght = r - l;
+                maxLength = currentLenght > maxLength ? currentLenght : maxLength;
+                l = l + substring.indexOf(chars[i]) + 1;
+            }
+        }
+
+        return maxLength > 0 ? r - l : maxLength;
+    }
+
+    public int lengthOfLongestSubstring1(String s) {
         int result = 0;
         int startIndex = 0;
         main:for (int i = 0; i < s.length(); i++) {
